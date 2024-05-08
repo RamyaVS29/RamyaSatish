44069e5a06204062143b83215d407fd119350cc2
Ramya Sathish <ramyas@infinite.com>
2024-05-08 18:23:02 +0530

    MyFirstCommit

---
+++ WebApplication1/WebApplication1/App_Start/RouteConfig.cs
@@ -0,0 +1,40 @@
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace WebApplication1
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            // Route for Electronics category
+            routes.MapRoute(
+                name: "Electronics",
+                url: "Electronics",
+                defaults: new { controller = "Home", action = "Electronics" }
+            );
+
+            // Route for Clothing category
+            routes.MapRoute(
+                name: "Clothing",
+                url: "Clothing",
+                defaults: new { controller = "Home", action = "Clothing" }
+            );
+
+            // Route for Shoes category
+            routes.MapRoute(
+                name: "Shoes",
+                url: "Shoes",
+                defaults: new { controller = "Home", action = "Shoes" }
+            );
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
